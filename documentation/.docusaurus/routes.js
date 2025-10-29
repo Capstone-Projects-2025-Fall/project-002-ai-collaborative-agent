c34d43de7ccffaae48233d80610096e51f461c0f
225Doul <tuo68092@temple.edu>
2025-10-29 18:12:32 -0400

    Add Jira feature implementation and supporting files

---
+++ documentation/.docusaurus/routes.js
@@ -0,0 +1,293 @@
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/project-002-ai-collaborative-agent/__docusaurus/debug',
+    component: ComponentCreator('/project-002-ai-collaborative-agent/__docusaurus/debug', 'a84'),
+    exact: true
+  },
+  {
+    path: '/project-002-ai-collaborative-agent/__docusaurus/debug/config',
+    component: ComponentCreator('/project-002-ai-collaborative-agent/__docusaurus/debug/config', 'e51'),
+    exact: true
+  },
+  {
+    path: '/project-002-ai-collaborative-agent/__docusaurus/debug/content',
+    component: ComponentCreator('/project-002-ai-collaborative-agent/__docusaurus/debug/content', '919'),
+    exact: true
+  },
+  {
+    path: '/project-002-ai-collaborative-agent/__docusaurus/debug/globalData',
+    component: ComponentCreator('/project-002-ai-collaborative-agent/__docusaurus/debug/globalData', '55d'),
+    exact: true
+  },
+  {
+    path: '/project-002-ai-collaborative-agent/__docusaurus/debug/metadata',
+    component: ComponentCreator('/project-002-ai-collaborative-agent/__docusaurus/debug/metadata', 'fe0'),
+    exact: true
+  },
+  {
+    path: '/project-002-ai-collaborative-agent/__docusaurus/debug/registry',
+    component: ComponentCreator('/project-002-ai-collaborative-agent/__docusaurus/debug/registry', 'af8'),
+    exact: true
+  },
+  {
+    path: '/project-002-ai-collaborative-agent/__docusaurus/debug/routes',
+    component: ComponentCreator('/project-002-ai-collaborative-agent/__docusaurus/debug/routes', '4da'),
+    exact: true
+  },
+  {
+    path: '/project-002-ai-collaborative-agent/api',
+    component: ComponentCreator('/project-002-ai-collaborative-agent/api', '1f4'),
+    exact: true
+  },
+  {
+    path: '/project-002-ai-collaborative-agent/markdown-page',
+    component: ComponentCreator('/project-002-ai-collaborative-agent/markdown-page', '5b6'),
+    exact: true
+  },
+  {
+    path: '/project-002-ai-collaborative-agent/docs',
+    component: ComponentCreator('/project-002-ai-collaborative-agent/docs', 'c07'),
+    routes: [
+      {
+        path: '/project-002-ai-collaborative-agent/docs',
+        component: ComponentCreator('/project-002-ai-collaborative-agent/docs', '803'),
+        routes: [
+          {
+            path: '/project-002-ai-collaborative-agent/docs',
+            component: ComponentCreator('/project-002-ai-collaborative-agent/docs', '655'),
+            routes: [
+              {
+                path: '/project-002-ai-collaborative-agent/docs/api-specification/calculator-model-generated',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/api-specification/calculator-model-generated', '862'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/api-specification/design-api-intro',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/api-specification/design-api-intro', 'c7e'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/api-specification/openapi-spec',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/api-specification/openapi-spec', 'f8d'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/category/api-specification',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/category/api-specification', '180'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/category/requirements-specification',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/category/requirements-specification', '712'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/category/system-architecture',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/category/system-architecture', '1e3'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/category/test-procedures',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/category/test-procedures', 'b0e'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/intro',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/intro', 'a75'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/requirements/features-and-requirements',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/requirements/features-and-requirements', 'b74'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/requirements/general-requirements',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/requirements/general-requirements', 'fb4'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/requirements/system-block-diagram',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/requirements/system-block-diagram', 'd92'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/requirements/system-overview',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/requirements/system-overview', '9f7'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/requirements/use-case-descriptions',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/requirements/use-case-descriptions', '8eb'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/system-architecture/design',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/system-architecture/design', '70c'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/system-architecture/development-environment',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/system-architecture/development-environment', 'f9d'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/system-architecture/version-control',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/system-architecture/version-control', '123'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/testing/acceptence-testing',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/testing/acceptence-testing', 'e86'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/testing/integration-testing',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/testing/integration-testing', 'da6'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/docs/testing/unit-testing',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/docs/testing/unit-testing', '3c2'),
+                exact: true,
+                sidebar: "docsSidebar"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '/project-002-ai-collaborative-agent/tutorial',
+    component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial', 'e0a'),
+    routes: [
+      {
+        path: '/project-002-ai-collaborative-agent/tutorial',
+        component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial', '110'),
+        routes: [
+          {
+            path: '/project-002-ai-collaborative-agent/tutorial',
+            component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial', '809'),
+            routes: [
+              {
+                path: '/project-002-ai-collaborative-agent/tutorial/category/custom-components',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial/category/custom-components', 'ce3'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/tutorial/category/tutorial---basics',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial/category/tutorial---basics', 'ad5'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/tutorial/category/tutorial---extras',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial/category/tutorial---extras', '086'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/tutorial/custom-components/figure',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial/custom-components/figure', '8da'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/tutorial/intro',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial/intro', 'bd7'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/tutorial/tutorial-basics/congratulations',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial/tutorial-basics/congratulations', '9e0'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/tutorial/tutorial-basics/create-a-document',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial/tutorial-basics/create-a-document', '756'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/tutorial/tutorial-basics/create-a-page',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial/tutorial-basics/create-a-page', '02e'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/tutorial/tutorial-basics/deploy-your-site',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial/tutorial-basics/deploy-your-site', '4c3'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/tutorial/tutorial-basics/markdown-features',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial/tutorial-basics/markdown-features', 'a1c'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/tutorial/tutorial-basics/mermaid',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial/tutorial-basics/mermaid', '2f9'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/tutorial/tutorial-basics/set-environment-variables',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial/tutorial-basics/set-environment-variables', 'ac8'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/tutorial/tutorial-extras/manage-docs-versions',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial/tutorial-extras/manage-docs-versions', 'f04'),
+                exact: true,
+                sidebar: "docsSidebar"
+              },
+              {
+                path: '/project-002-ai-collaborative-agent/tutorial/tutorial-extras/translate-your-site',
+                component: ComponentCreator('/project-002-ai-collaborative-agent/tutorial/tutorial-extras/translate-your-site', '7bc'),
+                exact: true,
+                sidebar: "docsSidebar"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '/project-002-ai-collaborative-agent/',
+    component: ComponentCreator('/project-002-ai-collaborative-agent/', '65b'),
+    exact: true
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*'),
+  },
+];
